cb4f050a0f1cef216de1a736449680f01ecc9e5e
Margaret Li <68254837+DesignProgrammer2020@users.noreply.github.com>
2021-11-19 16:38:16 -0800

    changed #qs to display to 8; changed questions
    will change bkg colors and text style&font later

--- sketch.js
+++ sketch.js
@@ -1,24 +1,31 @@
 let animating = false;
 
 //display random questions from this array
-let question_library = ["What outdoor activities do you do in summer?",
-"What indoor activities do you do in summer?",
-"What are your favorite barbecue meats/veggies?",
-"What are your favorite summer drinks?",
-"What are your favorite ice cream flavors?",
-"What are your favorite popsicle/snow cone flavors?",
-"What do you like to do at beaches?",
-"What is your ideal summer vacation destination?",
-"Where are you going/where did you go for vacation?",
-"What is your favorite nature/amusement park to visit in summer?",
-"Which kinds of museums do you like to visit?",
-"What kind of songs do you listen to in summer?",
-"What are your favorite summer sports?",
-"Which colors do you wear in summer?",
-"What do you like about summer sceneries?",
-"How to say “summer” in languages you know?",
-"How do you stay cool in summer?",
-"What do you like/dislike about summer?"];
+let question_library = [
+"What are one or two things you are thankful for this year?"
+"What is your favorite thing about Thanksgiving?"
+"What activities do you do during Thanksgiving?"
+"How to say “thank you” in your language?"
+
+"How often do you travel during Thanksgiving break?"
+"What plans do you have for this break/next break?"
+
+"Which places have you explored this year?"
+
+"What are your favorite Thanksgiving foods?"
+"What are your favorite Thanksgiving beverages?"
+"What are your favorite Thanksgiving desserts?"
+
+"How is the weather in your area?"
+"What do you like about the autumn season?"
+
+"Which movies have you watched recently?"
+"What kinds of music have you been listening to recently?"
+"What are your favorite music genres?"
+"What musical instrument do you play?"
+"What sports do you play/watch?"
+"What are your favorite sports teams?"
+];
 
 let index = -1;
 let state = 'title';
@@ -93,7 +100,7 @@ function buttonPressed(){
   if (index >= 0){
     questions.splice(index, 1);
   }
-  if (numQuestions >= 6){
+  if (numQuestions >= 8){
     initTitle();
     console.log(`question_library=${question_library}`);
     return;
